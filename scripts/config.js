cb54330da4dbcf070b3eb6c7fb8c3da82c19d473
theripper93 <tsplab@gmail.com>
2025-05-07 19:53:55 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -5,7 +5,7 @@ import { EmojifyApp } from "./emojifyApp.js";
 export function initConfig() {
     game.keybindings.register(MODULE_ID, "toggleEmoji", {
         name: game.i18n.localize(`${MODULE_ID}.toggleEmoji`),
-        editable: [{key: "KeyV", modifiers: [KeyboardManager.MODIFIER_KEYS.SHIFT]}],
+        editable: [{key: "KeyV", modifiers: [foundry.helpers.interaction.KeyboardManager.MODIFIER_KEYS.SHIFT]}],
         restricted: false,
         onDown: () => {
             new EmojifyApp().render(true);
